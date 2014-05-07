90c4c76b699e79cd20a50fd15621aff2c14f3131
Tommy Ho <tommy@bevyup.com>
2014-05-06 17:33:40 -0700

    Updating the Roster Widget to include an example for the updateParticipantInfo API

--- widgetsDemoV2/RosterWidget.js
+++ widgetsDemoV2/RosterWidget.js
@@ -1,12 +1,12 @@
 ﻿(function ($) {
-   
+
     // Register the callback to the init script
     window.initCallbackHooks = window.initCallbackHooks || [];
     window.initCallbackHooks.push(initCallback);
 
     // Member variables to store model references
     var m_SessionModel;
-    
+
     // This callback initializes the widget by getting the SessionModel
     function initCallback(isInSession) {
         if (isInSession) {
@@ -34,14 +34,14 @@
     function getSessionModelCallbackFailure(err) {
         log("(error) getSessionModelCallbackFailure: " + err);
     }
-    
+
     // This function configures the HTML of the widget with the information about the localParticipant
     // and if there are remoteParticipants, it will add the first one's information
     function appendWidgetHtmlToDom(localParticipant, remoteParticipants, parent) {
         var container = document.createElement('div');
         container.className = "bevyupIdentificationWidget";
 
-        var html = 
+        var html =
 "<h2>User Identity</h2>\
 <div class='content'>\
     <div class='welcomeText'>Welcome, <span class='welcomeUserName'>" + localParticipant.getName() + "</span>!</div>\
@@ -62,14 +62,69 @@
 
             var remotePartHtml =
 "<div class='welcomeText'>RemoteUser</div>\
-<div>Name: " + remoteParticipant.getName() + "</div>\
+<div id='nameDisplay'>Name: " + remoteParticipant.getName() + "</div>\
+<div id='emailDisplay'>Email: " + remoteParticipant.getEmail() + "</div>\
 <div>Link: " + window.location.origin + window.location.pathname + "?overrideInviteId=" + remoteParticipant.getInviteId() + "</div>";
-            
-            remoteParticipantContainer.innerHTML = remotePartHtml;
-            container.appendChild(remoteParticipantContainer);
+
+            if (localParticipant.getIsAgent()) {
+                // If the local user is an Agent, expose the ability to edit the name and email
+                remotePartHtml +=
+"<button class='remotePartEdit'>Edit</button>\
+<form class='remotePartEditForm' style='display:none'>\
+    Name: <input id='nameInput' type='text'><br>\
+    Email: <input id='emailInput' type='text'><br>\
+    <input class='remotePartEditSubmit' type='submit' value='Submit'></input>\
+</form>";
+
+                remoteParticipantContainer.innerHTML = remotePartHtml;
+                container.appendChild(remoteParticipantContainer);
+
+                // Hook up the event handlers for the edit agent related participant info functionality
+                container.querySelector(".remotePartEdit").addEventListener("click", editRemoteParticipant);
+                $(container).find(".remotePartEditForm").submit(editRemoteParticipantSubmit);
+            }
+            else {
+                remoteParticipantContainer.innerHTML = remotePartHtml;
+                container.appendChild(remoteParticipantContainer);
+            }
         }
 
         parent.appendChild(container);
+    }
+
+    // User clicked edit, show the edit form
+    function editRemoteParticipant() {
+        // Get the first remote participant
+        var part = m_SessionModel.getRemoteParticipants()[0];
+        $("#nameInput").val(part.getName());
+        $("#emailInput").val(part.getEmail());
+        $(".remotePartEditForm").show();
+    }
+
+    // User clicked submit. if there is a change, write the changes to the API
+    function editRemoteParticipantSubmit() {
+        var nameInputValue = $("#nameInput").val();
+        var emailInputValue = $("#emailInput").val();
+
+        // Get the first remote participant
+        var part = m_SessionModel.getRemoteParticipants()[0];
+        if (nameInputValue != part.getName() || emailInputValue != part.getEmail()) {
+
+            // Update the participant information with the participantModel
+            part.updateParticipantInfo({ "name": nameInputValue, "email": emailInputValue })
+                .done(function () {
+                    $("#nameDisplay").text("Name: " + part.getName());
+                    $("#emailDisplay").text("Email: " + part.getEmail());
+                    $(".remotePartEditForm").hide();
+                }).fail(function (err) {
+                    log("(error) updateParticipantInfo: " + err);
+                });
+        }
+        else {
+            $(".remotePartEditForm").hide();
+        }
+
+        return false;
     }
 
     // When a user clicks logout, this widget will call the leaveSession function on the BevyUpApi.
@@ -87,12 +142,12 @@
     function leaveSessionCallbackFailure(err) {
         log("(error) leaveSessionCallbackFailure: " + err);
     }
-    
+
     // Utility function to log to console with a prefix
     function log(s) {
         if (window.console) {
             console.log("BevyUp Sample> " + s);
         }
     }
-    
+
 }(jQuery));
